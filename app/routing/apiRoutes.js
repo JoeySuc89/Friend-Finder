0a93788187874a093f1460d2ee5f81acf3f133cd
JoeySuc89 <jsucameli1@gmail.com>
2019-05-17 22:44:41 -0400

    closing in on sorting the users and such

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,8 +8,17 @@ module.exports = function(app){
 
   app.post("/api/friends", function(req, res) {
     var currentFriend = [];
-    for (var i = 0; i < friendsArray.length; i++){
-      for (var j = 0; j < friends.scores.length);
+    for (var i = 0; i < friends.length; i++){
+      var difference = 0;
+      for (var j = 0; j < friends.scores.length; j++){
+       difference += Math.abs(friends.scores[j] - this.body.req.scores);
+      }
+     var num = 50;
+     if (difference < num) {
+       
+     }
     };
   });
 };
+
+// req.body,
