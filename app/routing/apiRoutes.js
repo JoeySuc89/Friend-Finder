516f45d35177d87b64e85077486faf77005c84e0
Joseph <jsucameli1@gmail.com>
2019-05-20 21:41:57 -0400

    made some comments through out to understand my code better

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,6 +17,8 @@ module.exports = function(app){
     var userData = req.body;
     var userName = userData.name;
     var userScores = userData.scores;
+    
+    //Here we go through each item in one array and are parsing them from strings and storing them as integers into the another array 
 
     var b = userScores.map(function(Item){
       return parseInt(Item, 10);
@@ -27,15 +29,15 @@ module.exports = function(app){
       scores: b
     };
 
-    var sum = b.reduce((a,b) => a + b, 0);
+    var sum = b.reduce((a,b) => a + b, 0); //Adding up the sum of all the userScores
 
     for (var i = 0; i < friends.length; i++){
      console.log(friends[i].name);
       totalDifference = 0;  
       
-      var bfriendScore = friends[i].scores.reduce((a,b) => a + b, 0);
+      var bfriendScore = friends[i].scores.reduce((a,b) => a + b, 0); //Here we are ading up the sum of all the friends scores
       console.log("Total friend score " + bfriendScore);
-      totalDifference += Math.abs(sum - bfriendScore);
+      totalDifference += Math.abs(sum - bfriendScore); // Here is is getting the difference between the user and all the friends scores
       console.log("------------------++++++++++++++++++++")
 
       if (totalDifference <= bestMatch.friendDifference) {
